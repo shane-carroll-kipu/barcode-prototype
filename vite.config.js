995dcae501bb2fc9b50abeaa9d1a757c8f098d68
Shane Carroll <shane.carroll@kipuhealth.com>
2025-10-03 16:26:14 -0400

    trying to get this going

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Needed for GitHub Pages project site: https://<user>.github.io/<repo>/
+  base: '/barcode-prototype/',
 })
