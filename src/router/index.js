189ea09ed9d7427a1490ccd20d26a802142f0b49
Shane Carroll <shane.carroll@kipuhealth.com>
2025-10-03 16:47:12 -0400

    fix gh site

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Landing from '../views/Landing.vue'
 import MedicationInput from '../views/MedicationInput.vue'
 import Rounds from '../views/Rounds.vue'
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/barcode-prototype/'),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
